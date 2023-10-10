bb7e88367567ea9d2754b100c3a31ed107965c98
verafes <verafes@gmail.com>
2023-10-10 15:36:11 -0700

    Update: Professor class updated

--- src/main/java/vfuapp/Professor.java
+++ src/main/java/vfuapp/Professor.java
@@ -1,5 +1,20 @@
 package vfuapp;
 
-public class Professor {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor extends Academic {
     private String id = "P";
+    private static int professorID = 1000001;
+    private static List<Professor> professors = new ArrayList<>();
+
+    public Professor(String firstName, String lastName, List<String> courses) {
+        super(firstName, lastName, courses);
+        this.id = id + professorID;
+        professorID++;
+    }
+
+    public void addProfessor(Professor professor) {
+        professors.add(professor);
+    }
 }
